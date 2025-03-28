eed32ce4d1ad3f01477c3455ff69c9636210022b
gabymachado10 <160601434+gabymachado10@users.noreply.github.com>
2025-03-28 09:41:45 -0300

    Add files via upload

--- tropicaliacerto-main/README.md
+++ tropicaliacerto-main/README.md
@@ -1,1 +1,16 @@
-# tropicaliacerto
+# Site acessivel sobre Tropicália 
+# Sobre
+Refatoração de um site implementando 
+recursos de acessibilidade no html, css
+e JS
+## Recursos de acessibilidasde 
+- Atributos aria 
+- alt
+- tab-index 
+- menu de acessibilidade 
+## Tecnologias utilizadas 
+- Bootstrap
+- ScroolRevealjs
+- HTML
+- CSS
+- JS
--- tropicaliacerto-main/tropicaliacerto-main/script.js
+++ tropicaliacerto-main/tropicaliacerto-main/script.js
@@ -25,9 +25,3 @@ document.addEventListener('DOMContentLoaded', function () {
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
   })
  })
-
-
- ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
- ScrollReveal().reveal('#galeria', { delay: 500 });
- ScrollReveal().reveal('#contato', { delay: 500 });
